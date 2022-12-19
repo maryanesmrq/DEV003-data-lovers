2610eb18df30e7c62513e842a9fa314ae472ec42
Fernanda Treviño <117880355+ftrevinoag@users.noreply.github.com>
2022-12-19 17:58:59 -0600

    Just completed tests and updated data.js

--- src/data.js
+++ src/data.js
@@ -1,47 +1,28 @@
-// estas funciones son de ejemplo
 
 export const filterByGeneration = (data, generation) => (data.filter((pokemon) => (
   pokemon.generation.name === generation))
-);
-
+); 
 
 export const search = (data, inputText) => {
-  const result = [];
-  // buscador de pokemones
-  data.forEach((eachPokemon) => {
-    const namePokemon = eachPokemon.name;
-    const lengthText = inputText.length;
-    if (namePokemon.slice(0, lengthText) === inputText) {
-      result.push(eachPokemon);
-    }
-  });
-  return result;
+  const lengthText = inputText.length;
+  return data.filter(
+    (pokemon) => pokemon.name.slice(0, lengthText) === inputText
+  )
 };
 
-export const filterByType = (pokemonType, typeChose) => {
-  const typesResult = [];
-  for (let i = 0; i < pokemonType.length; i += 1) {
-    for (let z = 0; z < pokemonType[i].type.length; z += 1) {
-      if (pokemonType[i].type[z] === typeChose) {
-        typesResult.push(pokemonType[i]);
-      }
-    }
-  }
-  return typesResult;
-};
+export const filterByType = (data, typeChose) => {
+  return data.filter(
+    (pokemon) => pokemon.type.includes(typeChose)
+  )
+}
+
+
 
 export const order = (data, parameter) => {
   let newArray = [];
-  let result = 0;
   // Orden alfabético ascendiente y descendiente
   const sortArray = data.sort((a, b) => {
-    if (a.name > b.name) {
-      result = 1;
-    }
-    if (a.name < b.name) {
-      result = -1;
-    }
-    return result;
+    return a.name > b.name ? 1 : -1
   });
   if (parameter === 'A-Z') {
     newArray = sortArray;
@@ -50,13 +31,7 @@ export const order = (data, parameter) => {
     // Orden por defecto
   } else {
     newArray = data.sort((a, b) => {
-      if (a.num > b.num) {
-        result = 1;
-      }
-      if (a.num < b.num) {
-        result = -1;
-      }
-      return result;
+      return a.name > b.name ? 1 : -1
     });
   }
   return newArray;
@@ -67,6 +42,3 @@ export const order = (data, parameter) => {
 
 
 
-export const anotherExample = () => {
-  return 'OMG';
-};
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,353 @@
-import { example, anotherExample } from '../src/data.js';
+import { search, order, filterByGeneration, filterByType } from '../src/data.js';
+import { data } from './testing-data'
+
+// Se crea un array que contenga
+
+const toEqual3 = [
+  {
+    num: '001',
+    name: 'bulbasaur',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'grass',
+      'poison',
+    ],
+  },
+];
+
+const toEqual4 = [
+  {
+    num: '024',
+    name: 'arbok',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'poison',
+    ],
+  },
+  {
+    num: '001',
+    name: 'bulbasaur',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'grass',
+      'poison',
+    ],
+  },
+  {
+    num: '005',
+    name: 'charmeleon',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fire',
+    ],
+  },
+  {
+    num: '173',
+    name: 'cleffa',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fairy',
+    ],
+  },
+  {
+    num: '200',
+    name: 'misdreavus',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'ghost',
+    ],
+  },
+  {
+    num: '060',
+    name: 'poliwag',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'water',
+    ],
+  },
+];
+
+const toEqual5 = [
+  {
+    num: '060',
+    name: 'poliwag',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'water',
+    ],
+  },
+  {
+    num: '200',
+    name: 'misdreavus',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'ghost',
+    ],
+  },
+  {
+    num: '173',
+    name: 'cleffa',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fairy',
+    ],
+  },
+  {
+    num: '005',
+    name: 'charmeleon',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fire',
+    ],
+  },
+  {
+    num: '001',
+    name: 'bulbasaur',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'grass',
+      'poison',
+    ],
+  },
+  {
+    num: '024',
+    name: 'arbok',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'poison',
+    ],
+  },
+];
+
+const toEqual6 = [
+  {
+    num: '001',
+    name: 'bulbasaur',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'grass',
+      'poison',
+    ],
+  },
+  {
+    num: '005',
+    name: 'charmeleon',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fire',
+    ],
+  },
+  {
+    num: '024',
+    name: 'arbok',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'poison',
+    ],
+  },
+  {
+    num: '060',
+    name: 'poliwag',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'water',
+    ],
+  },
+  {
+    num: '173',
+    name: 'cleffa',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'fairy',
+    ],
+  },
+  {
+    num: '200',
+    name: 'misdreavus',
+    generation: {
+      num: 'generation ii',
+      name: 'johto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'ghost',
+    ],
+  },
+];
+const toEqual7 = [
+  {
+    num: '001',
+    name: 'bulbasaur',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'grass',
+      'poison',
+    ],
+  },
+  {
+    num: '024',
+    name: 'arbok',
+    generation: {
+      num: 'generation i',
+      name: 'kanto',
+    },
+    pokemonRarity: 'normal',
+    type: [
+      'poison',
+    ],
+  },
+];
 
 
-describe('example', () => {
+
+describe('filterByGeneration', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterByGeneration).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('should return the right amount of items in array',()=>{
+    expect(filterByGeneration(data, 'johto').length).toEqual(2)
+  })
+
+  it('should return an empty array when search its not matching whith the entered letters', () => {
+    expect(filterByGeneration(data, 'xxwe')).toEqual([]);
+  });
+
+  it('should return two specific pokemons', () => {
+    const searchResult = filterByGeneration(data, 'johto')
+    expect(searchResult[0].name).toEqual('cleffa');
+    expect(searchResult[1].name).toEqual('misdreavus');
+  });
+
+  it('should have all returned pokemons be from johto region only',()=>{
+    const searchResult = filterByGeneration(data, 'johto')
+    const regionsFound = searchResult.map(el => el.generation.name)
+    const onlyUnique = regionsFound.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    })
+    expect(onlyUnique).toEqual(['johto']);
+  })
+})
+
+describe('search', () => {
+  it('is a function', () => {
+    expect(typeof search).toBe('function');
+  });
+
+  it('should return an empty array when search its not matching whith the entered letters', () => {
+    expect(search(data, 'xxwe')).toEqual([]);
+  });
+
+  it('should return a pokemon when the first letters are matching', () => {
+    expect(search(data, 'bul')).toEqual(toEqual3);
   });
 });
 
-
-describe('anotherExample', () => {
+// Alphabetical order
+describe('order', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof order).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  
+  it('should return a complete array from A to Z', () => {
+    expect(order(data, 'A-Z')).toEqual(toEqual4);
+  });
+
+  it('should return a complete array from Z to A', () => {
+    expect(order(data, 'Z-A')).toEqual(toEqual5);
+  });
+
+  it('should return an upward complete ordered array', () => {
+    expect(order(data, 'default')).toEqual(toEqual6);
   });
 });
+
+// Filter by type
+describe('filterByType', () => {
+  it('is a function', () => {
+    expect(typeof filterByType).toBe('function');
+  });
+
+  it('should return an array with same-type-pokemons', () => {
+    expect(filterByType(data, 'poison')).toEqual(toEqual7);
+  });
+});
+
+// eliminar toEqual
