35439a94eb960dfb50f05e7651d3b10eccf63d93
Fernanda Treviño <117880355+ftrevinoag@users.noreply.github.com>
2023-01-05 12:03:19 -0600

    done functions, modal, updated main, updated css

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+//import pokemon from "./data/pokemon/pokemon";
 
 export const filterByGeneration = (data, generation) => (data.filter((pokemon) => (
   pokemon.generation.name === generation))
@@ -17,12 +18,33 @@ export const filterByType = (data, typeChose) => {
 }
 
 export const order = (data, parameter) => {
-  if(parameter == 'A-Z' || parameter == 'Z-A'){
+  if(parameter === 'A-Z' || parameter === 'Z-A'){
     const sortedArray = data.slice().sort((a, b) => a.name > b.name ? 1 : -1 );
     return parameter === 'Z-A' ? sortedArray.reverse() : sortedArray
   } else {
     return data
   }
+};
+
+export const dpsCalculate = (quick, pokemonType) => {
+  let dps = 0;
+  const baseDamage = Number(quick['base-damage']);
+  const time = Number(quick['move-duration-seg']);
+  dps = (baseDamage / time).toFixed(1);
+  for (let i = 0; i < pokemonType.length; i += 1) {
+    if (pokemonType[i] === quick.type) {
+      let stab = 0;
+      stab = baseDamage + ((20 * baseDamage) / 100);
+      dps = (stab / time).toFixed(1);
+    }
+  }
+  return dps;
+};
+
+export const epsCalculate = (quick) => {
+  const energy = Number(quick.energy);
+  const time = Number(quick['move-duration-seg']);
+  return (energy / time).toFixed(1);
 };
 
     
